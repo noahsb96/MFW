c25b35dff8ec17bb1258a90c58ba1df00899dbd4
noahsb96 <noahsb96@yahoo.com>
2022-11-27 21:31:42 -0700

    Added videos page

--- mfw-crud/frontend/src/App.js
+++ mfw-crud/frontend/src/App.js
@@ -12,6 +12,7 @@ import Tour from "./components/MFW-Pages/Tour-Page/Tour";
 import About from "./components/MFW-Pages/About-Page/About";
 import Music from "./components/MFW-Pages/Music-Page/Music";
 import Photos from "./components/MFW-Pages/Photos-Page/Photos";
+import Videos from "./components/MFW-Pages/Videos-Page/Videos";
 
 class App extends Component {
   render() {
@@ -28,6 +29,7 @@ class App extends Component {
           <Route path="/about" element={<About />} />
           <Route path="/music" element={<Music />} />
           <Route path="/photos" element={<Photos />} />
+          <Route path="/videos" element={<Videos />} />
         </Routes>
       </>
     );
