566832af9e3e83fcd85d822c8c75cb791b16725d
noahsb96 <noahsb96@yahoo.com>
2022-12-17 18:44:26 -0700

    Implementing ChakraUI

--- mouth-for-war/src/components/MFW-Pages/Home-Page/Home.js
+++ mouth-for-war/src/components/MFW-Pages/Home-Page/Home.js
@@ -4,6 +4,8 @@ import Footer from "../../Footer/Footer";
 import { ShopContext } from "../../../context/shopContext";
 import { Link } from "react-router-dom";
 import Cart from "../../Cart/Cart";
+import { Box, Grid, Text, Image } from "@chakra-ui/react";
+
 const Home = () => {
   const { fetchAllProducts, products } = useContext(ShopContext);
 
@@ -26,22 +28,23 @@ const Home = () => {
         width="100%"
       ></img>
       <h1 id="merch-header">Merch</h1>
-      <div id="merch-container">
-        {products.slice(0, 6).map((product) => (
-          <div id="merch-div">
+      <Box>
+        <Grid templateColumns="repeat(3, 1fr)">
+          {products.slice(0, 6).map((product) => (
             <Link to={`/merch/${product.handle}`} key={product.title}>
-              <img id="product-image" src={product.images[0].src}></img>
-            </Link>
-            <div id="product-info">
-              <h3>{product.title}</h3>
-              <h3 id="price">
+            <Box _hover={{ opacity: '80%'}} textAlign="left">
+              <Image id="product-image" src={product.images[0].src} />
+
+              <Text>{product.title}</Text>
+              <Text>
                 ${product.variants[0].price.amount[0]}
                 {product.variants[0].price.amount[1]}
-              </h3>
-            </div>
-          </div>
-        ))}
-      </div>
+              </Text>
+              </Box>
+            </Link>
+          ))}
+        </Grid>
+      </Box>
       <Footer />
     </div>
   );
--- mouth-for-war/src/index.js
+++ mouth-for-war/src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import ShopProvider from "./context/shopContext";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ShopProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ShopProvider>
+    <ChakraProvider>
+      <ShopProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ShopProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
